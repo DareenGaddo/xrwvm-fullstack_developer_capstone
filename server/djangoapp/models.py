af439c804466164e2bc7a99abc896f51c1430af6
DareenGaddo <40184991+DareenGaddo@users.noreply.github.com>
2025-01-14 00:21:26 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -32,3 +32,52 @@ class CarModel(models.Model):
 
     def __str__(self):
         return f"{self.make} {self.name} ({self.year})"
+       
+# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+
+# All fields are from dealerships document in cloudant
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
+
+
+# <HINT> Create a plain Python class `DealerReview` to hold review data
+
+# All fields are from review documet in cloudant
+class DealerReview:
+
+    def __init__(self, id, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year, sentiment='neutral'):
+        self.id = id
+        self.name = name  # reviewer name
+        self.dealership = dealership
+        self.review = review  # reveiw text
+        self.purchase = purchase  # Did the reviewer purchase the car? bool
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.sentiment = sentiment  # Watson NLU sentiment analysis of review
+
+        def __str__(self):
+            return 'Reviewer: ' + self.name + ' Review: ' + self.review
